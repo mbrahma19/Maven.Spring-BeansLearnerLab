63bcd83548162a044ce12549000ee2754adcf9fd
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-27 18:46:56 -0400

    Saving generated files

---
+++ src/main/java/com/learnerlab/learnerlab/LearnerlabApplication.java
@@ -0,0 +1,13 @@
+package com.learnerlab.learnerlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnerlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnerlabApplication.class, args);
+	}
+
+}
